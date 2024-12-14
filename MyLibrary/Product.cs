f4bc7bd1965cd1c872d8b49fb771127b33c3b715
radorado <radorado@OFP-TEC-21835>
2024-12-14 11:52:31 -0500

    Creacion de Clase Product2

--- MyLibrary/Product.cs
+++ MyLibrary/Product.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace MyLibrary
 {
-    public class Product (int id, string name, decimal unitPrice, int unitsInStock)
+    public class Product(int id, string name, decimal unitPrice, int unitsInStock)
     {
         // Propiedades
-        public int Id { get; }
-        public string Name { get;}
-        public decimal UnitPrice { get; }
-        public int UnitsInStock { get; }
+        public int Id => id;
+        public string Name => name;
+        public decimal UnitPrice => unitPrice;
+        public int UnitsInStock => unitsInStock;
 
+    }
+
+    public class Product2(int id, string name,
+        bool discontinued)
+        : Product(id, name, default, default)
+    {
+        public bool Discontinued { get; set; } = discontinued;
     }
 }
